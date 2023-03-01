31665ae99d434269a55b53e5c801688884d7a4f4
amine-mui <live.and.learn.svj@gmail.com>
2023-03-01 11:58:19 +0100

    Admin (PlaceHolder)

---
+++ front/StackNav.jsx
@@ -0,0 +1,17 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen, ProfileScreen }  from './components/HomeProfileScreen.jsx';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ front/components/HomeProfileScreen.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const ProfileScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+export { HomeScreen, ProfileScreen };
