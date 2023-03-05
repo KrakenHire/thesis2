a45ffa3179fc96825fce90dca6b1e0f481733e93
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2023-03-05 12:21:12 +0100

    no changes

--- front/App.js
+++ front/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View ,Image,TouchableOpacity} from 'react-native';
@@ -7,12 +6,6 @@ import ProviderProfile from './components/ProviderProfile.js';
 // import icons from './assets/icons/index';
 // import Zendesk from 'react-native-zendesk-messaging';
 // const CHANNEL_KEY = 'eyJzZXR0aW5nc191cmwiOiJodHRwczovL2doYWRhMjI2Ny56ZW5kZXNrLmNvbS9tb2JpbGVfc2RrX2FwaS9zZXR0aW5ncy8wMUdURUJRUVQ4Sk00UTY5MDVTNlpFWTM5NS5qc29uIn0=';
-=======
-
-
-
-import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
 import Onboardscreen from './Component/Onboardscreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -22,8 +15,11 @@ import LoginScreenUser from './Component/LoginScreenUser.js';
 import forgetPass from "./Component/forgetPass.js"
 import SignUpPro from "./Component/SignUpPro.js"
 import HomeScreenPro from './Component/HomeScreenPro.js';
+
+
+
+
 const Stack = createNativeStackNavigator();
->>>>>>> 26ba19683e57e548a738f8f4122c5882f421ab85
 
 export default function App() {
 
@@ -47,19 +43,17 @@ export default function App() {
   // )
 
   return (
-<<<<<<< HEAD
  
-    <View style={styles.container}>
-      {/* <Home/>  */}
-   <ProviderProfile/> 
+//     <View style={styles.container}>
+//       {/* <Home/>  */}
+//    <ProviderProfile/> 
      
-     <StatusBar style="auto" /> 
-     {/* {ChatBotbn()} */}
-    </View>
-);
-      }
+//      <StatusBar style="auto" /> 
+//      {/* {ChatBotbn()} */}
+//     </View>
+// );
+//       }
 
-=======
     <NavigationContainer style={styles.container}>
        <Stack.Navigator>
     
@@ -77,7 +71,6 @@ export default function App() {
   
   );
 }
->>>>>>> 26ba19683e57e548a738f8f4122c5882f421ab85
 //hello
 const styles = StyleSheet.create({
   container: {
@@ -86,28 +79,25 @@ const styles = StyleSheet.create({
     margin:0,
     alignItems: 'center',
     justifyContent: 'center',
-<<<<<<< HEAD
  
    
   }, 
     
-    btn :{
-      position: 'absolute',
-      botton:20,
-      right: 10,
-      height:60 ,
-      width: 60,
-      backgroundColor: 'white',
-      borderRadius: 30,
-          }
-        ,
-        bot : {
-          height:50 ,
-      width: 50,
+  //   btn :{
+  //     position: 'absolute',
+  //     botton:20,
+  //     right: 10,
+  //     height:60 ,
+  //     width: 60,
+  //     backgroundColor: 'white',
+  //     borderRadius: 30,
+  //         }
+  //       ,
+  //       bot : {
+  //         height:50 ,
+  //     width: 50,
       
-        }
-=======
-  },
+  //       }
+  // },
   
->>>>>>> 26ba19683e57e548a738f8f4122c5882f421ab85
 });
